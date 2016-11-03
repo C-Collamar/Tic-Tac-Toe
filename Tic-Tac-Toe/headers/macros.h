6f3a421975d83140d9ef2a3b4939a50c21afdd6c
C-Collamar <christian.collamar@gmail.com>
2016-11-03 23:50:10 +0800

    Update macros.h

--- Tic-Tac-Toe/headers/macros.h
+++ Tic-Tac-Toe/headers/macros.h
@@ -1,9 +1,11 @@
 #ifndef MACROS_H_
 #define MACROS_H_
 
-#define BOARD_HEIGHT 5
-#define BOARD_WIDTH 4
+#define BOARD_HEIGHT 3
+#define BOARD_WIDTH 3
+
 #define BLANK_CHARACTER '?'
 #define ALIGNMENT_NUMBER 3 //this defines the number of adjacent X's and O's in order to win the game
+#define TREE_DEPTH 3 //this defines the thinking level of the minimax algorithm
 
 #endif /* MACROS_H_ */
