85e381f3291e68778f990dbdf4b9ff77acea37ad
C-Collamar <christian.collamar@gmail.com>
2016-11-04 19:51:15 +0800

    Update init.h

--- Tic-Tac-Toe/headers/Memory Management/init.h
+++ Tic-Tac-Toe/headers/Memory Management/init.h
@@ -2,39 +2,32 @@
 #define INIT_H_
 
 // This function allocates memory for 'Player'-type objects
-void InitPlayer(Player **player) {
-	*player = malloc(sizeof(Player));
-	if(*player == NULL) {
+void InitPlayer(Player *player) {
+	player = malloc(sizeof(Player));
+	if(player == NULL) {
 		printf("ERROR: Memory allocation failed.\n");
 		exit(EXIT_FAILURE);
 	}
 }
 
-/* This function allocates memory to the board of size BOARD_HEIGHT * BOARD_WIDTH
- * and initializes all elements to BLANK_CHARACTER
- */
-void InitBoard(char ***board) {
-	int x,y;
+//this function initializes the board elements to BLANK_CHARACTER
+void InitBoard(GameBoard *board) {
+	int x, y;
 
-	*board = (char**)malloc(BOARD_HEIGHT * sizeof(char *));
-	for(x = 0; x < BOARD_HEIGHT; ++x) {
-		(*board)[x] = (char *)malloc(BOARD_WIDTH * sizeof(char));
-
-		for(y = 0; y < BOARD_WIDTH; ++y) {
-			(*board)[x][y] = BLANK_CHARACTER;
-		}
-	}
+	for(x = 0; x < BOARD_HEIGHT; ++x)
+		for(y = 0; y < BOARD_WIDTH; ++y)
+			board->state[x][y] = BLANK_CHARACTER;
 }
 
-void InitGame(Player **computer, Player **user) {
+void InitGame(Player *computer, Player *user) {
 	/* the following lines initialize the numeric representation
 	 * for distinguishing user's turn from computer's turn
 	 */
-	(*computer)->turn = (userPlaysFirst())? 0 : 1;
-	(*user)->turn = ((*computer)->turn == 1)? 0 : 1;
+	computer->turn = (userPlaysFirst())? 0 : 1;
+	user->turn = (computer->turn == 1)? 0 : 1;
 
-	(*computer)->symbol = 'x';
-	(*user)->symbol = 'o';
+	computer->symbol = 'x';
+	user->symbol = 'o';
 }
 
 // This function initializes the initial possible moves both players can make
