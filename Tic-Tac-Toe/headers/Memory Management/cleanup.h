7607f77387265f39d850d137aaf5515498a2441d
C-Collamar <christian.collamar@gmail.com>
2016-11-07 19:38:48 +0800

    Update cleanup.h

--- Tic-Tac-Toe/headers/Memory Management/cleanup.h
+++ Tic-Tac-Toe/headers/Memory Management/cleanup.h
@@ -1,19 +1,7 @@
 #ifndef CLEANUP_H_
 #define CLEANUP_H_
 
-void destroyPlayer(Player *player) {
-	free(player);
-	player = NULL;
-}
-
 void destroyBoard(GameBoard *board) {
-	int x;
-
-	for(x = 0; x < BOARD_WIDTH; ++x) {
-		free(board->state[x]);
-	}
-
-	free(board->state);
 	free(board);
 }
 
