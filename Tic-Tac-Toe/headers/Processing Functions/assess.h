74309b0e38e321d08ba7756ac276b76941637f8a
C-Collamar <christian.collamar@gmail.com>
2016-11-07 19:37:03 +0800

    Update assess.h

--- Tic-Tac-Toe/headers/Processing Functions/assess.h
+++ Tic-Tac-Toe/headers/Processing Functions/assess.h
@@ -95,14 +95,135 @@ int checkForWinner(GameBoard board, int *lastMove) {
 	return 0;
 }
 
-/* PARAMETERS:
- * move - an array of 2 elements defining the AI's move - row and column
- * legalMoves - a collection of all possible moves and scoring values for those moves
- * board - the board used for analyzing the current game state
- * depthCount - keeps track on the current depth of the game tree
+/* This function checks if a tie has occurred. It checks every square on the board if
+ * there is still a blank character. Return 0 if there is, or 1 meaning there isn't
  */
-void findBestMove(int *move, LinkedList *legalMoves, GameBoard board, int depthCount) {
-	printf("(Under development)\n");
+int tie(GameBoard board) {
+	int x = -1, y;
+
+	while(++x < BOARD_HEIGHT) {
+		y = -1;
+		while(++y < BOARD_WIDTH)
+			if(board.state[x][y] == BLANK_CHARACTER)
+				return 0;
+	}
+
+	//if it reaches to this point where every square in the board is filled up
+	return 1;
+}
+
+/* DESCRIPTION: This function rates the move based on the board
+ * state and the number of turns it takes to win or not loose
+ *
+ * PARAMETERS:
+ *  - board: the current board state
+ *  - move: the move to be applied on the current board state
+ *  - legalMoves: the list of valid moves left
+ *  - treeDepth: the height of the tree
+ *               Indicates the number of times this function has been recursed, and who's turn will it be
+ *               Its initial value passed is zero, which means we know that it's the computer's turn if treeDepth % 1 equals zero
+ */
+int minimax(GameBoard board, int *move, int treeDepth) {
+	printf("TREE DEPTH: %i\n", treeDepth);
+
+	if(treeDepth > TREE_DEPTH) {
+		return 0;
+	}
+
+	//first, execute the input move by placing the symbol of the computer/ user, to the board
+	updateBoard(&board, move, ((treeDepth & 1) == 0)? computer.symbol : user.symbol);
+	displayBoard(board);
+
+	//if executing that move results to winning
+	if(checkForWinner(board, move) == 1) {
+		/* return 1 if it's the computer was the one who won in this scenario.
+		 * Otherwise, return -1, which tells the computer that this is a bad news for him (or her haha)
+		 */
+		return ((treeDepth & 1) == 0)? 1 : -1;
+	}
+
+	//also check if that move resulted in a tie
+	if(tie(board))
+		return 0;
+
+	//if not, create a list of possible moves
+	LinkedList *legalMoves = getPossibleMoves(board);
+
+	//assign a a pointer to point at each node of legal moves
+	LinkedList *currentLegalMove = legalMoves;
+
+	//get the scores for every possible move
+	while(currentLegalMove != NULL) {
+		currentLegalMove->score = minimax(board, currentLegalMove->coordinate, ++treeDepth);
+		currentLegalMove = currentLegalMove->next;
+	}
+
+	currentLegalMove = legalMoves;
+	int finalScore = legalMoves->score;
+
+	//if it's the computer's move that the computer thinks of
+	if((treeDepth & 1) == 0) {
+		//get the highest score among the scores, since the computer wants to maximize its chance of winning
+		while(currentLegalMove != NULL) {
+			if(currentLegalMove->score > finalScore)
+				finalScore = currentLegalMove->score;
+			currentLegalMove = currentLegalMove->next;
+		}
+	}
+	//else if it's the user's turn that the computer thinks of
+	else {
+			//get the lowest score, since the user wants to minimize the computer's chances of winning
+			while(currentLegalMove != NULL) {
+				if(currentLegalMove->score < finalScore)
+					finalScore = currentLegalMove->score;
+				currentLegalMove = currentLegalMove->next;
+			}
+		}
+
+	//deallocate the list properly and return the final score for the node which this function was called
+	destroyLinkedList(&legalMoves);
+	return finalScore;
+}
+
+/* PARAMETERS:
+ * - move: an array of 2 elements defining the AI's move - row and column
+ * - legalMoves: a collection of all possible moves and scoring values for those moves
+ * - board: the board used for analyzing the current game state
+ * - depthCount: keeps track on the current depth of the game tree
+ */
+void findBestMove(int *move, GameBoard board) {
+	//create a list of legal moves
+	LinkedList *legalMoves = getPossibleMoves(board);
+
+	//create a pointer to each legal-move node
+	LinkedList *currentLegalMove = legalMoves;
+
+	//for each legal moves, determine their score. Higher score indicates better move
+	while(currentLegalMove != NULL) {
+		//the minimax function rates the move based on the board and the number of turns it takes to win or not loose
+		currentLegalMove->score = minimax(board, currentLegalMove->coordinate, 0);
+
+		//after rating that move, move on to the next
+		currentLegalMove = currentLegalMove->next;
+	}
+
+	//reset 'currentLegalMove' to point at the start of the list of legal moves
+	currentLegalMove = legalMoves;
+
+	//declare a variable to compare scores and initialize it to the score of the fist legal move
+	int highestScore = legalMoves->score;
+
+	//find the best move among the rated legal moves by finding the highest score among them
+	while(currentLegalMove != NULL) {
+		if(currentLegalMove->score > highestScore) {
+			highestScore = currentLegalMove->score;
+			move[0] = currentLegalMove->coordinate[0];
+			move[1] = currentLegalMove->coordinate[1];
+		}
+		currentLegalMove = currentLegalMove->next;
+	}
+
+	destroyLinkedList(&legalMoves);
 }
 
 #endif /* ASSESS_H_ */
